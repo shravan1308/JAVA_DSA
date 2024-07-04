4d6f1d877a09e1b29bcf77d515941835d7842e8e
Shravan Bhagat <bhagatshravan011cs2005@gmail.com>
2024-07-04 14:04:33 +0530

    This is the encapsulation That gives the java security from other class access without permission.

---
+++ encapuslation/EncapsulationIntroduction.java
@@ -0,0 +1,14 @@
+package encapuslation;
+
+public class EncapsulationIntroduction {
+    public static void main(String[] args) {
+        
+    Student obj = new Student();
+    obj.setAge(34);
+    obj.setName("kamolika");
+            System.out.println(obj.getAge());
+            System.out.println(obj.setName()); 
+    obj.setLife(32);
+    }
+    
+}
---
+++ encapuslation/Student.java
@@ -0,0 +1,30 @@
+package encapuslation;
+
+public class Student {
+    private int life;
+    // private String name1;
+     private int age;
+    private String name;
+    // getter and setter method making
+    public int getAge(){
+        return age;
+    }
+    // 8888888888888888//
+    public int getLife(){
+        return life;
+    }
+    public void setLife(int life){
+        this.life = life;
+    }
+    public String setName(){
+        return name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    
+}
